8cb8e111b0b89f99a7d4e9af008dc1344d8cdc34
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-12-04 00:40:25 +0300

    Add volume levels

--- src/script.js
+++ src/script.js
@@ -4,6 +4,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 
+import GUI from 'lil-gui'
+import Microphone from './Microphone'
+
 
 /**
  * Base
@@ -13,7 +16,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color( 0x132020 )
+scene.background = new THREE.Color(0.3,0.0,0.0 ) //0x132020 
 /**
  * Sizes
  */
@@ -55,13 +58,15 @@ controls.enableDamping = true
  * sphere
  */
 
-const sphereGeo = new THREE.SphereGeometry( 5, 32, 32 )
+const sphereGeo = new THREE.SphereGeometry( 8, 64, 64 )
 const sphereMaterial = new THREE.ShaderMaterial({
 
     vertexShader:vertexShader,
     fragmentShader:fragmentShader,
     uniforms:{
-        uTime:{value:0}
+        uTime:{value:0},
+        uBigWaveElevation:{value:1},
+        uFresnelMultiplier:{value:0.5}
     }
 })
 
@@ -69,125 +74,50 @@ const sphere = new THREE.Mesh(sphereGeo,sphereMaterial)
 
 scene.add(sphere)
 
-/**
- * Platform
- */
-const plane = new THREE.PlaneGeometry(12,12)
-const material = new THREE.MeshBasicMaterial( {color: 0x000478F, side: THREE.DoubleSide} )
-const platform = new THREE.Mesh(plane,material)
-scene.add(platform)
-platform.position.set(0,-8,0)
-platform.rotation.x = 8
 
 /**
  * Audio
  */
+const microphone = new Microphone()
 
-let ready = false
-let volume = 0
-let levels = []
-let stream = null
-
-
-
-let analyserNode = {}
-analyserNode.fftSize = 256
-let byteFrequencyData = null
-let floatTimeDomainData = null
-const initAudio = ()=>{
-
-    const audioContext = new AudioContext()
-    const mediaStreamSourceNode = audioContext.createMediaStreamSource(stream)
-    analyserNode = audioContext.createAnalyser()
-
-    mediaStreamSourceNode.connect(analyserNode)
-    floatTimeDomainData = new Float32Array(analyserNode.fftSize)
-    byteFrequencyData = new Uint8Array(analyserNode.fftSize)
-
-    ready = true
-
-}
-let spectrum= {}
-    spectrum.width = analyserNode.fftSize
-    spectrum.height = 128
-    spectrum.halfHeight = Math.round(spectrum.height *0.5)
-    spectrum.canvas = document.createElement('canvas')
-    spectrum.canvas.width = spectrum.width
-    spectrum.canvas.height = spectrum.height
-    spectrum.canvas.style.position = 'fixed'
-    spectrum.canvas.style.left = 0
-    spectrum.canvas.style.bottom = 0
-    spectrum.context = spectrum.canvas.getContext('2d')
-    spectrum.context.fillStyle = '#ffffff'
-document.body.append(spectrum.canvas)
-
-spectrum.update = () =>
+//Volume
+let variations = {}
+variations.volume = {}
+variations.volume.target = 0
+variations.volume.current = 0
+variations.volume.upEasing = 0.03
+variations.volume.downEasing = 0.002
+variations.volume.getValue = () =>
 {
-    spectrum.context.clearRect(0, 0, spectrum.width, spectrum.height)
+    const level0 = microphone.levels[0] || 0
+    const level1 = microphone.levels[1] || 0
+    const level2 = microphone.levels[2] || 0
 
-    for(let i = 0; i < analyserNode.fftSize; i++)
-    {
-        // const floatTimeDomainValue = floatTimeDomainData[i]
-        const byteFrequencyValue = byteFrequencyData[i]
-        const normalizeByteFrequencyValue = byteFrequencyValue / 255
-
-        const x = i
-        const y = spectrum.height - (normalizeByteFrequencyValue * spectrum.height)
-        const width = 1
-        // const height = floatTimeDomainValue * spectrum.height
-        const height = normalizeByteFrequencyValue * spectrum.height
-
-        spectrum.context.fillRect(x, y, width, height)
-    }
-    
+    return Math.max(level0, level1, level2) * 3.0
+}
+variations.volume.getDefault = () =>
+{
+    return 0.152
 }
 
-navigator.mediaDevices
-    .getUserMedia({audio:true,video:false})
-    .then((_stream)=>{
-        stream = _stream
-
-        initAudio()
-        
-    })
-
-const getLevels=()=>
+//Medium Level
+variations.lowLevel = {}
+variations.lowLevel.target = 0
+variations.lowLevel.current = 0
+variations.lowLevel.upEasing = 0.008
+variations.lowLevel.downEasing = 0.004
+variations.lowLevel.getValue = () =>
 {
-    const bufferLength = analyserNode.fftSize
-    const levelCount = 8
-    const levelBins = Math.floor(bufferLength / levelCount)
+    let value = microphone.levels[0] || 0
+    value *= 4
+    value += 0.25
+    value = Math.max(0.25, value)
 
-    
-    let max = 0
-    
-    for(let i = 0; i < levelCount; i++)
-    {
-        let sum = 0
-
-        for(let j = 0; j < levelBins; j++)
-        {
-            sum +=  byteFrequencyData[(i * levelBins) + j]
-        }
-
-        const value = sum / levelBins / 256
-        levels[i] = value
-
-        if(value > max)
-            max = value
-    }
-
-    return levels
+    return value
 }
-
-const getVolume=()=>
+variations.lowLevel.getDefault = () =>
 {
-    let sumSquares = 0.0
-    for(const amplitude of floatTimeDomainData)
-    {
-        sumSquares += amplitude * amplitude
-    }
-    volume = Math.sqrt(sumSquares / floatTimeDomainData.length)
-    return volume
+    return 0.25
 }
 
 
@@ -202,9 +132,14 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+/**
+ * GUI
+ */
+// const gui = new GUI()
+
+// gui.add(sphereMaterial.uniforms.uBigWaveElevation, 'value').min(0.01).max(1.5).step(0.01).name('uBigWaveElevation')
 
 
-console.log(spectrum)
 /**
  * Animate
  */
@@ -220,21 +155,12 @@ const animateScene = () =>
     //Update shader with time
     sphereMaterial.uniforms.uTime.value = elapsedTime
 
+    sphereMaterial.uniforms.uBigWaveElevation.value = variations.volume.getValue()
+     sphereMaterial.uniforms.uFresnelMultiplier.value = variations.lowLevel.getValue()
+    //  console.log(variations.lowLevel.getValue())
     //Update Microphone
-    if(ready)
-    {
-        // Retrieve audio data
-        analyserNode.getByteFrequencyData(byteFrequencyData)
-        analyserNode.getFloatTimeDomainData(floatTimeDomainData)
-        
-        volume = getVolume()
-        levels = getLevels()
-
-        if(spectrum){
-            spectrum.update()
-        } 
-        
-    }
+    microphone.update()
+    
     // Update controls
     controls.update()
 
